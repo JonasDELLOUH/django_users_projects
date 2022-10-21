01514c20d928cbb3adb00d3661dee67b96d3b30b
= <jdellouh1@gmail.com>
2022-10-21 09:53:35 +0200

    aaa

---
+++ api/views.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from api.serializers import ProjectSerializer
+from projects.models import Project
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {'GET': 'api/projects'},
+        {'POST': 'api/users/token'}
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getProjects(request):
+    projects = Project.objects.all()
+    serialize = ProjectSerializer(projects, many=True)
+    return Response(serialize.data)
---
+++ projects/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+
+
+# Create your models here.
+from users.models import Profile
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
